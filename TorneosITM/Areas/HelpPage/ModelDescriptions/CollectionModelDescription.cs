e4c4be7149e05e4ebc2e78a9498c5e0f41832729
CAROLINA ARANGO <carolinaarango1114665@correo.itm.edu.co>
2025-04-23 10:51:10 -0500

    Creacion del proyecto en VS

---
+++ TorneosITM/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TorneosITM.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TorneosITM/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TorneosITM.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
